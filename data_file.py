81f9be939dcd4f6f5ac0075873616caf8da7eb7b
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2019-11-05 21:49:01 -0500

    added data file
    The merged text file has the close

---
+++ data_file.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+# We can pull technical indicators as well from alpha_vantage using alpha_vantage.technicalindicators
+
+# The equities we want
+tickers = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'FB']
+# Your key here
+key = 'TUFG6CJ46YJKR38I'
+# Chose your output format, or default to JSON (python dict)
+ts = TimeSeries(key, output_format='pandas')
+
+stocks = {}
+for equity in tickers:
+    stocks[equity] = ts.get_daily(symbol=equity)[0]
+
+# The TimeSeries function returns a tuple, where the first item in the tuple is the pandas data frame with the
+# historical data
+# aapl_data is a pandas dataframe, aapl_meta_data is a dict
+# aapl_data, aapl_meta_data = ts.get_daily(symbol='AAPL', outputsize='full')
+
+# We want to keep the close prices only so the following chunk of code makes one pandas dataframe with just the closing
+# prices of all of the equities with their name. We can reset the index once we call merged.txt and set index to date to
+# create a time series plot.
+data_lst = []
+for key, item in stocks.items():
+    for name in item.columns:
+        if name != '4. close':
+            item = item.drop(name, axis=1)
+    item = item.rename(columns={'4. close': key})
+    data_lst.append(item.reset_index())
+df_merged = reduce(lambda left, right: pd.merge(left, right, on=['date'], how='outer'), data_lst)
+df_merged.to_csv('merged.txt', sep=',', na_rep='.', index=False)
