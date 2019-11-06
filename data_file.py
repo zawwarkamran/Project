4d86fc8543e85524c1aa6db240941ac022c30b2b
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2019-11-05 22:28:04 -0500

    added some notes

--- data_file.py
+++ data_file.py
@@ -10,16 +10,17 @@ key = 'TUFG6CJ46YJKR38I'
 ts = TimeSeries(key, output_format='pandas')
 
 # The TimeSeries function returns a tuple, where the first item in the tuple is the pandas data frame with the
-# historical data
+# historical data. Right now we are getting the 'compact' historical data, this basically means the last 100 days of
+# data, if we set outputsize to 'full' we can get all of the data alpha_vantage has.
 # aapl_data is a pandas dataframe, aapl_meta_data is a dict
 # aapl_data, aapl_meta_data = ts.get_daily(symbol='AAPL', outputsize='full')
 stocks = {}
 for equity in tickers:
-    stocks[equity] = ts.get_daily(symbol=equity)[0]
+    stocks[equity] = ts.get_daily(outputsize='compact', symbol=equity)[0]
 
 # We want to keep the close prices only so the following chunk of code makes one pandas dataframe with just the closing
 # prices of all of the equities with their name. We can reset the index once we call merged.txt and set index to date to
-# create a time series plot.
+# create a time series plot, although tbh this is not needed.
 data_lst = []
 for key, item in stocks.items():
     for name in item.columns:
@@ -27,5 +28,5 @@ for key, item in stocks.items():
             item = item.drop(name, axis=1)
     item = item.rename(columns={'4. close': key})
     data_lst.append(item.reset_index())
-df_merged = reduce(lambda left, right: pd.merge(left, right, on=['date'], how='outer'), data_lst)
+df_merged = reduce(lambda left, right: pd.merge(left, right, on=['date'], how='outer'), data_lst).fillna(0)
 df_merged.to_csv('merged.txt', sep=',', na_rep='.', index=False)
