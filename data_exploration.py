c123a14986666da578850240f3e18f3ebdb446dd
Zawwar Kamran <51718813+zawwarkamran@users.noreply.github.com>
2019-11-05 22:01:49 -0500

    scatter plot and heatmap of correlation, as well as individual time series plots

---
+++ data_exploration.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('white')
+
+data = pd.read_csv('merged.txt', sep=',')
+data.set_index('date', inplace=True)
+
+# Shows the individual time series for each equity
+for item in data.columns:
+    data[item].plot()
+    plt.title(item)
+    plt.tight_layout()
+    plt.ylabel('Price')
+    plt.grid()
+    plt.show()
+
+# Shows the correlation between each equity
+sns.heatmap(data.corr(), cmap='coolwarm', annot=True)
+plt.show()
+
+sns.pairplot(data)
+plt.show()
